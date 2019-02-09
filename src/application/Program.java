d3d36ca874113c3ffa0f4fa00fdd91b9892a0f73
Dennathor <reis.pl1997@gmail.com>
2019-02-09 18:57:11 -0200

    lista

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import java.util.ArrayList;
+
 public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		ArrayList <String> lista = new ArrayList<>();
 
 	}
 
