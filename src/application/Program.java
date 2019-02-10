72e2514e5d59b558c2702af138236028838853d9
Dennathor <reis.pl1997@gmail.com>
2019-02-09 22:05:16 -0200

    AA

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		ArrayList <String> lista = new ArrayList<>();
+		ArrayList <int> lista = new ArrayList<>();
 
 	}
 
