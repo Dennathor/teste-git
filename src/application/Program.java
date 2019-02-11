54f94e6ce80d1435f34e376898db2e67d9666578
Dennathor <reis.pl1997@gmail.com>
2019-02-11 10:42:13 -0200

    Arrumei

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		ArrayList <int> lista = new ArrayList<>();
+		ArrayList <Integer> lista = new ArrayList<>();
 
 	}
 
