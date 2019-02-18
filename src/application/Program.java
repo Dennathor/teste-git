9ad921be41db05fdb5bf9c95e0faa74db90be7cf
Dennathor <reis.pl1997@gmail.com>
2019-02-17 23:28:16 -0300

    FAD

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		int prob = 100;
+		String nome = "AAF";
 		System.out.println("Hello World!");
 		ArrayList <Integer> lista = new ArrayList<>();
-
 	}
 
 }
